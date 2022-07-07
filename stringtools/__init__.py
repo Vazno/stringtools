856b84511aaeaf98c45f1fbd2c98dea16e573318
Beksultan <96925396+Vazno@users.noreply.github.com>
2022-07-08 00:20:17 +0600

    renamed function bundle

--- stringtools/__init__.py
+++ stringtools/__init__.py
@@ -1,4 +1,4 @@
 ''' MIT License Copyright (c) 2022 Beksultan Artykbaev'''
-from .analyse import is_pangram, count_char
+from .analysers import is_pangram, count_char
 from .converters import bricks
 from .generators import generate_nick, Generate_password
